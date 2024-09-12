5cd0cb77805c6e5f79dc77a6dcc7c2019b4cd0be
Samuel Eduardo <samueleduardorocha@gmail.com>
2024-09-12 15:31:41 -0300

    Criado o constrututor da class Assento e metodo mostraLugares

--- Assento.java
+++ Assento.java
@@ -1,4 +1,30 @@
 public class Assento {
 
-    
+    private boolean[][] disponivel;
+
+    public Assento() {
+        this.disponivel = new boolean[5][4];
+
+        for(int i = 0; i < disponivel.length; i++){
+            for(int j = 0; j < disponivel[i].length; j++){
+                this.disponivel[i][j] = true;
+            }
+        }
+    }
+
+    public String mostraLugares(){
+        String resultado = "";
+        for(int i = 0; i < disponivel.length; i++){
+            for(int j = 0; j < disponivel[i].length; j++){
+                if (disponivel[i][j]) {
+                    resultado += "O";
+                } else{
+                    resultado += "X";
+                }
+            }
+            resultado += "\n"; // Nova linha após cada fila
+        }
+        return resultado;
+    }
+
 }
