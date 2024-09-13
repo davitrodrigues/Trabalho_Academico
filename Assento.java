6997e6c6c31db3b538aac991e7411bca51779c1a
Samuel Eduardo <samueleduardorocha@gmail.com>
2024-09-12 21:55:23 -0300

    Adicionado array na classe Assento

--- Assento.java
+++ Assento.java
@@ -1,35 +1,42 @@
+import java.util.ArrayList;
+
 public class Assento {
 
-    private boolean[][] disponivel;
+    private ArrayList<ArrayList<Boolean>> disponivel;
 
-
-    /*Construtor da Class */
+    /* Construtor da Classe */
     public Assento() {
-        this.disponivel = new boolean[5][4];
+        this.disponivel = new ArrayList<>();
 
-        /*Aqui vai andar a matriz e iniciar tudo true (tudo disponivel) */
-        for(int i = 0; i < disponivel.length; i++){
-            for(int j = 0; j < disponivel[i].length; j++){
-                this.disponivel[i][j] = true;
+        // Inicializa a estrutura com 5 linhas e 4 assentos por linha
+        for (int i = 0; i < 5; i++) {
+            ArrayList<Boolean> linha = new ArrayList<>();
+            for (int j = 0; j < 4; j++) {
+                linha.add(true); // Assentos disponíveis inicialmente
             }
+            this.disponivel.add(linha);
         }
     }
 
-    /* Thiago E Davi, aqui temos o metodo que vai imprimir "O" caso disponivel o lugar (true) e "X" para lugar indisponivel (false) */
-
-    public String mostraLugares(){
+    /* Método que vai imprimir "O" caso disponível o lugar (true) e "X" para lugar indisponível (false) */
+    public String mostraLugares() {
         String resultado = "";
-        for(int i = 0; i < disponivel.length; i++){
-            for(int j = 0; j < disponivel[i].length; j++){
-                if (disponivel[i][j]) {
-                    resultado += "O";
-                } else{
-                    resultado += "X";
-                }
+        for (ArrayList<Boolean> linha : disponivel) {
+            for (Boolean assento : linha) {
+                resultado += (assento ? "O" : "X");
             }
             resultado += "\n"; // Nova linha após cada fila
         }
         return resultado;
     }
 
+    // Método para obter o status de um assento específico
+    public boolean getAssento(int linha, int assento) {
+        return disponivel.get(linha).get(assento);
+    }
+
+    // Método para definir o status de um assento específico
+    public void setAssento(int linha, int assento, boolean status) {
+        this.disponivel.get(linha).set(assento, status);
+    }
 }
